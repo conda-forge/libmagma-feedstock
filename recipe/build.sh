4a0941e030a9b374ccd00d87003caddd1555901f
Daniel Ching <carterbox@users.noreply.github.com>
2023-05-31 17:49:53 -0500

    Adjust CUDAARCHS

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,13 +4,19 @@ set -exv
 make generate --jobs ${CPU_COUNT}
 
 # Duplicate lists because of https://bitbucket.org/icl/magma/pull-requests/32
-export CUDA_ARCH_LIST="sm_35,sm_50,sm_60,sm_70,sm_80"
-export CUDAARCHS="35-virtual;50-virtual;60-virtual;70-virtual;80-virtual"
+export CUDA_ARCH_LIST="sm_35,sm_50,sm_60,sm_70,sm_75,sm_80"
+export CUDAARCHS="35-virtual;50-virtual;60-virtual;70-virtual;75-virtual;80-virtual"
 
 # Only build the lowest non-deprecated arch to minimize build time
 if [[ "$target_platform" == "linux-ppc64le" || "$target_platform" == "linux-aarch64" ]]; then
-  export CUDA_ARCH_LIST="sm_60"
-  export CUDAARCHS="60-virtual"
+  export CUDA_ARCH_LIST="sm_60;sm_80"
+  export CUDAARCHS="60-virtual;80-virtual"
+fi
+
+# Only build the lowest non-deprecated arch to minimize build time
+if [[ "$cuda_compiler_version" == "12.0" ]]; then
+  export CUDA_ARCH_LIST="sm_50,sm_60,sm_70,sm_75,sm_80,sm_89"
+  export CUDAARCHS="50-virtual;60-virtual;70-virtual;75-virtual;80-virtual;89-virtual"
 fi
 
 # Remove CXX standard flags added by conda-forge. std=c++11 is required to
