dc071b38561257e356d8708f46aae4bb9cfdddce
Daniel Ching <dching@nvidia.com>
2025-07-29 19:54:03 -0500

    BLD: Cleanup cuda arch lists

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,19 +11,15 @@ set -exv
 # 11.8 supports archs 3.5 - 9.0
 # 12.x supports archs 5.0 - 9.0
 
-# Duplicate lists because of https://bitbucket.org/icl/magma/pull-requests/32
 # Use the same arches as https://github.com/pytorch/pytorch/blob/07fa6e2c8b003319f85a469307f1b1dd73f6026c/.ci/magma/Makefile#L7
 # Only difference is 37 is replaced with 35.
 
-export CUDA_ARCH_LIST="sm_50,sm_60,sm_70,sm_80"
 export CUDAARCHS="50-real;60-real;70-real;80-real"
 
 if [[ "$cuda_compiler_version" == "11.8" ]]; then
-  export CUDA_ARCH_LIST="${CUDA_ARCH_LIST},sm_35,sm_86,sm_90"
   export CUDAARCHS="${CUDAARCHS};35-real;86-real;90"
 
 elif [[ "$cuda_compiler_version" == "12."* ]]; then
-  export CUDA_ARCH_LIST="${CUDA_ARCH_LIST},sm_86,sm_90,sm_100,sm_120"
   export CUDAARCHS="${CUDAARCHS};86-real;90-real;100-real;120"
 
 else
